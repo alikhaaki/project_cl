bd1b99c184d8c56f74791c94c87fc57f624f200e
a.khaksari <activharley@gmail.com>
2019-06-02 00:23:13 +0430

    a

--- app/src/main/java/ali/com/timelaps/IntroActivity.java
+++ app/src/main/java/ali/com/timelaps/IntroActivity.java
@@ -4,14 +4,15 @@ import Fragment.FragmentOne;
 import Fragment.FragmentThree;
 import Fragment.FragmentTwo;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
 import com.github.paolorotolo.appintro.AppIntro;
 
 public class IntroActivity extends AppIntro {
-
-
 
 
     @Override
@@ -26,6 +27,24 @@ public class IntroActivity extends AppIntro {
         setProgressButtonEnabled(true);
 
 
+        setColorSkipButton(ContextCompat.getColor(this, R.color.black));
+        setSkipText("رد شدن");
+        setDoneText("باشه !");
+        setBarColor(ContextCompat.getColor(this, R.color.grey_100));
+        setColorDoneText(ContextCompat.getColor(this,R.color.black));
+    }
+    @Override
+    public void onBackPressed(){
+    }
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
 
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
     }
 }
